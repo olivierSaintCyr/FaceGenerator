22b4822aff639f91db8fd06fa1702b765b606ea8
olivierSaintCyr <olivier@WhiteShadow.localdomain>
2021-01-14 15:48:59 -0500

    add_loss save and load method implementation in LossLog class

--- src/loss_log.py
+++ src/loss_log.py
@@ -5,14 +5,27 @@ class LossLog(object):
     def __init__(self):
         self.losses = {}
 
-    def add_loss(self):
-        pass
-
+    def __str__(self):
+        return self.losses.__str__()
+    
+    def add_loss(self, new_losses):
+        losses_keys = self.losses.keys()
+        for key in new_losses.keys():
+            if key not in losses_keys:
+                self.losses[key] = []
+                for loss in new_losses[key]:
+                    self.losses[key].append([loss])
+            else:
+                for i in range(len(self.losses[key])):
+                    self.losses[key][i].append(new_losses[key][i])
+        
     def save(self):
-        pass
+        with open('losses_log_dict.pickle', 'wb') as f:
+            pickle.dump(self.losses, f)
 
     def load(self):
-        pass
+        with open('train_info.pickle', 'rb') as f:
+            self.losses = pickle.load(f)
 
     def graph(self):
         pass
